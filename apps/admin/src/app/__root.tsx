f23943518eb6923bd020d2f3bcb72c974da2440b
koralle <koralle@koralle-mgmg.com>
2025-12-16 05:41:49 +0900

    feat: rename admin-ui to ui

--- apps/admin/src/app/__root.tsx
+++ apps/admin/src/app/__root.tsx
@@ -1,4 +1,4 @@
-import { UIProvider } from "@component-vista/admin-ui";
+import { UIProvider } from "@component-vista/ui";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
