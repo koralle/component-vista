77b0c4e411fa5d73ac1be11839f9718f69ca9a01
koralle <koralle@koralle-mgmg.com>
2025-12-16 05:44:23 +0900

    feat: rename admin-ui to ui (#10)

--- apps/admin/src/app/(auth)/sign-up.tsx
+++ apps/admin/src/app/(auth)/sign-up.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@component-vista/admin-ui";
+import { Button } from "@component-vista/ui";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(auth)/sign-up")({ component: RouteComponent });
