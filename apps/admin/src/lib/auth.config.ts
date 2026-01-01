5de9f01a5d5592b1bb74912f630750796e6090c9
koralle <koralle+github@koralle-mgmg.com>
2026-01-01 20:04:11 +0900

    feat: reset

--- apps/admin/src/lib/auth.config.ts
+++
@@ -1,8 +0,0 @@
-import type { BetterAuthOptions } from "better-auth/minimal";
-
-export const authConfig = {
-  emailAndPassword: {
-    enabled: true,
-    autoSignIn: false,
-  },
-} as const satisfies Omit<BetterAuthOptions, "database" | "secret">;
